60cca07cec368ba1efa4b30a78ee4b86c13d86c5
Brad Smith <brad@geekdome.net>
2016-01-25 12:29:31 -0500

    WIP: non-working first pass at a module model

--- app/pods/module/model.js
+++ app/pods/module/model.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
-
+ 
+var workspace_url_path = "/assets/workspace/"
 export default DS.Model.extend({
-  
+    path:	 DS.attr(),  // dir name relative to workspace_url_path
+    content: Ember.computed('path', function() {
+		var h5pjson_fn = workspace_url_path + "/" + this.get('path') + "/content/content.json";
+		return $.getJSON(h5pjson_fn).responseJSON;
+    })
 });
